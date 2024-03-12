717b4e3223b78af222af57622d0cd04345fa95a5
Timur Gaynulin <timur.gaynulin@gmail.com>
2024-03-12 22:24:12 +0300

    [homework_01] fix is_Prime

--- homework_01/main.py
+++ homework_01/main.py
@@ -27,6 +27,8 @@ PRIME = "prime"
 def is_prime(n):
     if n <= 1:
         return False
+    if n == 2:
+        return True
     if n % 2 == 0:
         return False
     for i in range(3, (n // 2)+1):
